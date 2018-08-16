bcf551f30354d82ae70a06b89a0991731f7df045
RR <raimondreest@hotmail.com>
2018-08-16 09:31:39 +0300

    Simple cmd listener

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(void)
 {
-	printf("Hello, World!\n");
+	printf("TODO LIST APPLICATION\n");
+
+	char cmd[8];
+
+	while (strcmp(cmd, "q\n") != 0) {
+		// Listen to the users input for a command
+		fgets(cmd, 8, stdin);
+		printf("[debug] cmd: %s", cmd);
+
+		if (strcmp(cmd, "c\n") == 0) {
+			printf("Creating new todo\n");
+		} else if (strcmp(cmd, "l\n") == 0) {
+			printf("Listing all todos\n");
+		}
+	}
+
+	printf("Exiting... Goodbye!");
 	return 0;
 }
